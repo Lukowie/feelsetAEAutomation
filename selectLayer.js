5b730465230cdc5fd24ed31e3319d0621ff95382
Lukowie <lukevdm64@gmail.com>
2023-04-02 02:05:59 -0400

    got all of the automated editing done, now to tweak the template and render

--- selectLayer.js
+++ selectLayer.js
@@ -1,10 +1,43 @@
+// get project
+var proj = app.project;
 
-var arr = [];
-var positions = [];
-for (var i=1; i < app.project.items.length; i++){
-    arr.push(app.project.item(i).name);
-    positions.push(app.project.activeItem.layer(i).position.value)
+// get comp item
+var comp = proj.item(4);
+
+// get folder
+// allows for the actual song file to be selected without needing the explicit name
+var folder = Folder("./Song").getFiles();
+
+// import file
+var item = proj.importFile(new ImportOptions(folder[0]));
+
+//get all layers in comp
+var layers = comp.layers;
+layers.add(item);
+// layer index is 1, once added
+
+// get music layer as AVItem object
+
+var musicLayer = comp.layer(1);
+
+// make sure music layer start time is 0.0 and inPoint is 0.0
+musicLayer.startTime = 0.0;
+musicLayer.inPoint = 0.0;
+
+
+var visualizerLayer = comp.layer(3);
+
+//alert(musicLayer.name);
+var outerEffect = visualizerLayer.Effects.property(1);
+var innerEffect = outerEffect.property("Audio Layer");
+innerEffect.setValue(1);
+//alert(visualizerLayer.effect.property(1).property(1).setAlternateSource());// = musicLayer;
+
+
+//get outPoint of Music layer and set all other layers to the outPoint
+for (var i=1; i <= comp.numLayers; i++){
+    comp.layer(i).outPoint = musicLayer.outPoint;
 }
+// At this point all layers are the same duration
 
-    alert(arr);
-    alert(positions);
+//alert("Music layer: " + musicLayer.name);
